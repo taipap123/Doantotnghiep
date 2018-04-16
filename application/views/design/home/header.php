b8e047564e6eaa8bd6c0471ecac7e0f4a6d99e6a
ngo tan tai <taipap123@gmail.com>
2018-04-16 15:27:10 +0700

    +Update View design (banner)

--- application/views/design/home/header.php
+++ application/views/design/home/header.php
@@ -1,5 +1,6 @@
 <div class="header">
 <div class="header_top">
+<div><img src="<?php echo base_url() ?>public/images/banner.jpg" alt=""></a></div>
 <div class="logo">
 <a href="index.html"><img src="<?php echo base_url() ?>public/images/logo.png" alt=""></a>
 </div>
@@ -24,7 +25,40 @@
     </ul>
 </div>
 <div class="header_bottom_img">
-<img src="<?php echo base_url() ?>public/images/header_bottom_img.jpg" alt="">
+<!--<img src="<?php echo base_url() ?>public/images/header_bottom_img.jpg" alt=""> -->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="<?php echo base_url() ?>public/images/banner.jpg" alt="...">
+      <div class="carousel-caption">
+      </div>
+    </div>
+    <div class="item">
+      <img src="<?php echo base_url() ?>public/images/banner.jpg" alt="...">
+      <div class="carousel-caption">
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </div>
 <div class="clear"></div>
 </div>
